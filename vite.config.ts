f4ce272e1b51f802b038747f763083dd54addd26
Karsten Schmidt <k@thi.ng>
2024-09-03 14:34:26 +0200

    build: update vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
-// import { createHtmlPlugin } from "vite-plugin-html";
+import { createHtmlPlugin } from "vite-plugin-html";
+import { resolve } from "node:path";
 
 export default defineConfig({
-	build: { target: "esnext" },
-	// plugins: [createHtmlPlugin({ minify: true })],
+	build: {
+		target: "esnext",
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				art: resolve(__dirname, "art.html"),
+			},
+		},
+	},
+	plugins: [createHtmlPlugin({ minify: true })],
 });
