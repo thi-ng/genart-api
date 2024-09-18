40ddd268274adbcfcc20d5bf5cee183d7e7e655b
Karsten Schmidt <k@thi.ng>
2024-09-18 18:15:19 +0200

    feat(examples): add GUI chooser

---
+++ src.examples/index.ts
@@ -0,0 +1,23 @@
+import { select, option } from "@thi.ng/hiccup-html";
+import { $clear, $compile } from "@thi.ng/rdom";
+import { launchEditorForms } from "./param-editor.js";
+import { launchEditorImgui } from "./param-editor-imgui.js";
+
+const app = document.getElementById("app")!;
+
+const loadGUI = (e: InputEvent) => {
+	$clear(app);
+	const id = +(<HTMLSelectElement>e.target).value;
+	if (!isNaN(id)) {
+		[launchEditorImgui, launchEditorForms][id]();
+	}
+};
+
+$compile(
+	select(
+		{ onchange: loadGUI },
+		option({}, "Choose GUI editor..."),
+		option({ value: 0 }, "@thi.ng/imgui"),
+		option({ value: 1 }, "@thi.ng/rdom-forms")
+	)
+).mount(app);
