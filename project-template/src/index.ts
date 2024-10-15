0d036f3d9bab78c2455ecfa42924674513d641a4
Karsten Schmidt <k@thi.ng>
2024-10-15 13:26:10 +0200

    feat(template): fix/update template code example

--- project-template/src/index.ts
+++ project-template/src/index.ts
@@ -1,17 +1,22 @@
 // import type { GenArtAPI } from "@thi.ng/genart-api";
 
-$genart.setParams({
+const param = await $genart.setParams({
 	color: $genart.params.color({
 		name: "Human readable name",
 		desc: "Brief description",
-		default: "#ff0000",
+		// if no default is provided, a random value will be used
+		// default: "#ff0000",
 	}),
 });
 
 $genart.setUpdate((time, frame) => {
 	// draw something...
 	console.log(time, frame);
+	// set background color to current param value
+	document.body.style.backgroundColor = param("color");
 
 	// must return true to continue anim loop
 	return true;
 });
+
+export {};
