62fdf388c92cbdf835bfb44e521777dbf91103b8
Karsten Schmidt <k@thi.ng>
2024-10-19 14:25:36 +0200

    build: update build-examples script

--- build-examples.js
+++ build-examples.js
@@ -1,6 +1,12 @@
 import { execFileSync } from "node:child_process";
 
-for (let ex of ["param-custom", "param-editors", "param-image", "param-test"]) {
+for (let ex of [
+	"p5-basic",
+	"param-custom",
+	"param-editors",
+	"param-image",
+	"param-test",
+]) {
 	console.log("building example:", ex);
 	try {
 		execFileSync("yarn", ["build"], {
