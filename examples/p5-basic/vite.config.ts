5973f7f096a0ec419fc7a9a7eaf151c927262c3b
Karsten Schmidt <k@thi.ng>
2024-10-19 14:59:49 +0200

    fix(examples): fix build setup for p5 example

--- examples/p5-basic/vite.config.ts
+++ examples/p5-basic/vite.config.ts
@@ -2,9 +2,5 @@ import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
-	build: {
-		target: "esnext",
-		// sourcemap: true,
-	},
 	plugins: [createHtmlPlugin({ minify: true })],
 });
