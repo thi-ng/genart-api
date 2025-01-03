3a3eb3dc9ae5d26d151666f90a34bef16c8d6627
Karsten Schmidt <k@thi.ng>
2025-01-03 20:24:49 +0100

    feat(adapter-fxhash): initial import as new pkg

---
+++ packages/adapter-fxhash/README.md
@@ -0,0 +1,134 @@
+# @genart-api/adapter-fxhash
+
+[GenArtAPI](https://github.com/thi-ng/genart-api/) platform adapter for the
+[**fx(hash)**](https://fxhash.xyz) art platform.
+
+See main [README](https://github.com/thi-ng/genart-api/blob/main/README.md) for
+details.
+
+## Usage
+
+```bash
+yarn add @genart-api/core @genart-api/adpater-fxhash
+
+# create dest dir
+mkdir -p lib
+
+# copy files
+cp node_modules/@genart-api/core/*.min.js lib
+cp node_modules/@genart-api/adapter-fxhash/*.min.js lib
+```
+
+If you're a TypeScript user, you'll also want to add the `types` field in your
+`tsconfig.json`:
+
+tsconfig.json:
+
+```json
+{
+	"compilerOptions": {
+		"types": ["@genart-api/core", "@genart-api/adapter-fxhash"]
+	}
+}
+```
+
+In your HTML wrapper, add the following script tags to the `<head>` to load the
+core `GenArtAPI` and the **EditArt** platform adapter:
+
+```html
+<script src="./lib/genart.min.js"></script>
+<script src="./lib/adapter-fxhash.min.js"></script>
+```
+
+See [related
+section](https://github.com/thi-ng/genart-api/blob/main/README.md#use-in-your-own-projects-an-artists-hello-world)
+in main project README for more details...
+
+Once running, you can then test your example in the fx(hash) sandbox:
+
+https://www.fxhash.xyz/sandbox
+
+## Parameter handling
+
+### Type adaptations & conversions
+
+> [!IMPORTANT]
+> Parameter adaptation for different platforms is fully invisible to the artwork
+> and no code changes need to be done in the artwork (which is the entire
+> purpose of platform adapters in this system).
+
+Because **fx(hash)** only supports a subset of the parameter types available in
+`GenArtAPI`, only the following types can be used for projects intended for this
+platform. Where possible, param types will be adapted. Params using any
+unsupported types will be skipped (i.e. not exposed to **fx(hash)**) and will
+only ever evaluate to their default values. When using such unsupported types,
+the platform adapter will log a warning message in the browser console.
+
+### Choice
+
+[Reference](https://github.com/thi-ng/genart-api/blob/main/README.md#choice-parameter)
+
+Will be represented as a **fx(hash)** `select` parameter.
+
+### Color
+
+[Reference](https://github.com/thi-ng/genart-api/blob/main/README.md#color-parameter)
+
+Will be represented as a **fx(hash)** `color` parameter.
+
+### Range
+
+[Reference](https://github.com/thi-ng/genart-api/blob/main/README.md#range-parameter)
+
+Will be represented as a **fx(hash)** `number` parameter.
+
+### Text
+
+[Reference](https://github.com/thi-ng/genart-api/blob/main/README.md#text-parameter)
+
+Will be represented as a **fx(hash)** `string` parameter.
+
+### Toggle
+
+[Reference](https://github.com/thi-ng/genart-api/blob/main/README.md#toggle-parameter)
+
+Will be represented as a **fx(hash)** `boolean` parameter.
+
+### Vector
+
+[Reference](https://github.com/thi-ng/genart-api/blob/main/README.md#vector-parameter)
+
+Will be represented as multiple **fx(hash)** `number` parameters, one per vector
+component.
+
+> [!IMPORTANT]
+> Your artwork will still use vectors as param value and the adapter
+> automatically reconciles any changes done to any of the adapted params on the
+> **fx(hash)** side.
+
+For example, a 3D vector param will be represented (on **fx(hash)**'s side) as
+three separate number params. When either of them are modified, this platform
+adapter will apply the changes to the correct vector component/index and
+propagate the changed vector value via the main `GenArtAPI` system.
+
+### Weighted choice
+
+[Reference](https://github.com/thi-ng/genart-api/blob/main/README.md#weighted-choice-parameter)
+
+Will be represented as a **fx(hash)** `select` parameter. Weights will be
+ignored when randomizing the param in the **fx(hash)** UI.
+
+### XY
+
+[Reference](https://github.com/thi-ng/genart-api/blob/main/README.md#xy-parameter)
+
+Similar to [vector params](#vector), XY params will also be represented as two
+**fx(hash)** `number`s.
+
+### Update behavior
+
+TODO write docs
+
+## License
+
+&copy; 2024 - 2025 Karsten Schmidt // MIT License
---
+++ packages/adapter-fxhash/src/index.ts
@@ -0,0 +1,357 @@
+import type {
+	BigIntParam,
+	BinaryParam,
+	ChoiceParam,
+	Param,
+	ParamSpecs,
+	ParamUpdateBehavior,
+	PlatformAdapter,
+	PRNG,
+	RangeParam,
+	ResizeMessage,
+	RunMode,
+	ScreenConfig,
+	TextParam,
+	Traits,
+	VectorParam,
+	WeightedChoiceParam,
+	XYParam,
+} from "@genart-api/core";
+
+declare global {
+	var fxhash: string;
+	var $fx: {
+		readonly hash: string;
+		readonly context: "standalone" | "capture" | "minting";
+		params(specs: any[]): void;
+		getParam(id: string): any;
+		features(features: Record<string, string | number | boolean>): void;
+		preview(): void;
+		on(
+			id: string,
+			pre: (...args: any[]) => boolean | Promise<boolean>,
+			post: (...args: any[]) => void
+		): void;
+	};
+}
+
+const TYPE_MAP: Record<
+	string,
+	"bigint" | "boolean" | "bytes" | "color" | "number" | "select" | "string"
+> = {
+	bigint: "bigint",
+	binary: "bytes",
+	choice: "select",
+	color: "color",
+	range: "number",
+	toggle: "boolean",
+	text: "string",
+	vector: "number",
+	weighted: "select",
+	xy: "number",
+};
+
+const UPDATE_MAP: Record<ParamUpdateBehavior, FxParam["update"]> = {
+	reload: "page-reload",
+	event: "sync",
+};
+
+const {
+	prng: { sfc32 },
+	utils: { equiv, isString, hashString },
+} = $genart;
+
+/**
+ * Adapter configuration options. To be used with
+ * {@link FxhashAdapterOpts.configure}.
+ */
+export interface FxhashAdapterOpts {}
+
+interface AdaptedParam {
+	/**
+	 * Original param ID
+	 */
+	id: string;
+	/**
+	 * Function to convert adapted param value to original type.
+	 *
+	 * @param value
+	 */
+	adapt(value: any): any;
+}
+
+interface FxParam {
+	id: string;
+	name: string;
+	type: string;
+	update?: "page-reload" | "sync" | "code-driven";
+	default?: any;
+	options?: any;
+}
+
+export class FxhashAdapter implements PlatformAdapter {
+	protected _searchParams: URLSearchParams;
+	protected _params?: ParamSpecs;
+	protected _cache: Record<string, any> = {};
+	protected _adaptations: Record<string, AdaptedParam> = {};
+	protected _prng!: PRNG;
+	protected _screen: ScreenConfig;
+
+	constructor() {
+		this._searchParams = new URLSearchParams(location.search);
+		this._screen = this.screen;
+		this.initPRNG();
+		$genart.on("genart:state-change", ({ state }) => {
+			if (state === "ready") $genart.start();
+		});
+		$fx.on(
+			"params:update",
+			(...args) => {
+				let [id, value] = Object.entries(args[0])[0];
+				const adaptedParam = this._adaptations[id];
+				if (adaptedParam) {
+					id = adaptedParam.id;
+					value = adaptedParam.adapt(value);
+				}
+				const param = this._params?.[id];
+				if (!param) {
+					this.warn(`ignoring change for unknown param: ${id}...`);
+					return false;
+				}
+				if (equiv(this._cache[id], value)) return false;
+				this._cache[id] = value;
+				if (param.update !== "reload") {
+					// only update param if no reload required
+					$genart.setParamValue(id, value);
+				}
+				return true;
+			},
+			() => {}
+			// (...args) => console.log("update post", args)
+		);
+		window.addEventListener("resize", () => {
+			const { width, height, dpr } = this._screen;
+			const newScreen = this.screen;
+			if (
+				width !== newScreen.width ||
+				height !== newScreen.height ||
+				dpr !== newScreen.dpr
+			) {
+				this._screen = newScreen;
+				$genart.emit<ResizeMessage>({
+					type: "genart:resize",
+					screen: newScreen,
+				});
+			}
+		});
+	}
+
+	get id() {
+		return "@genart-api/adapter-fxhash";
+	}
+
+	get mode() {
+		return (<Record<typeof $fx.context, RunMode>>{
+			standalone: "play",
+			capture: "preview",
+			minting: "edit",
+		})[$fx.context];
+	}
+
+	get screen() {
+		return {
+			width: window.innerWidth,
+			height: window.innerHeight,
+			dpr: window.devicePixelRatio || 1,
+		};
+	}
+
+	get prng() {
+		return this._prng;
+	}
+
+	configure(_: Partial<FxhashAdapterOpts>) {}
+
+	async updateParam(id: string, _: Param<any>) {
+		let value: any;
+		if (Object.values(this._adaptations).find((x) => x.id === id)) {
+			value = this._cache[id];
+			return { value };
+		} else {
+			value = $fx.getParam(id);
+		}
+		console.log(
+			`${this.id}:`,
+			id,
+			"new value",
+			value,
+			"cached",
+			this._cache[id]
+		);
+		if (value == null || equiv(this._cache[id], value)) return;
+		this._cache[id] = value;
+		return { value };
+	}
+
+	async initParams(params: ParamSpecs) {
+		this._params = params;
+		const fxParams = [];
+		for (let id in params) {
+			const src = params[id];
+			const type = TYPE_MAP[src.type];
+			if (!type) {
+				this.warn(
+					`unsupported type '${src.type}' for param id: ${id}, skipping...`
+				);
+				continue;
+			}
+			const dest: FxParam = {
+				id,
+				name: src.name,
+				type,
+				default: src.default,
+				update: UPDATE_MAP[src.update],
+			};
+			fxParams.push(dest);
+			this._cache[id] = src.default;
+			switch (src.type) {
+				case "bigint": {
+					const { min, max } = <BigIntParam>src;
+					dest.options = { min, max };
+					break;
+				}
+				case "binary": {
+					const { maxLength } = <BinaryParam>src;
+					dest.update = "code-driven";
+					dest.options = { length: maxLength };
+					break;
+				}
+				case "choice": {
+					const { options } = <ChoiceParam<any>>src;
+					dest.options = {
+						options: options.map((x) => (isString(x) ? x : x[0])),
+					};
+					break;
+				}
+				case "color": {
+					this._adaptations[id] = {
+						id,
+						adapt: (x) => (isString(x) ? x : x.hex.rgb),
+					};
+					if (dest.default) dest.default = dest.default.substring(1);
+					break;
+				}
+				case "range": {
+					const { min, max, step } = <RangeParam>src;
+					dest.options = { min, max, step };
+					break;
+				}
+				case "text": {
+					const { minLength, maxLength } = <TextParam>src;
+					dest.options = { minLength, maxLength };
+					break;
+				}
+				case "vector": {
+					// replace vector param with multiple number params
+					fxParams.pop();
+					const $src = <VectorParam>src;
+					const size = $src.size;
+					const labels = $src.labels;
+					for (let j = 0; j < size; j++) {
+						const $dest = { ...dest };
+						$dest.id = id + "__" + labels[j];
+						$dest.name = $src.name + ` (${labels[j]})`;
+						$dest.options = {
+							min: $src.min[j],
+							max: $src.max[j],
+							step: $src.step[j],
+						};
+						if ($src.default) $dest.default = $src.default[j];
+						fxParams.push($dest);
+						this._adaptations[$dest.id] = this.adaptVectorParam(
+							id,
+							j
+						);
+					}
+					break;
+				}
+				case "weighted": {
+					const { options } = <WeightedChoiceParam<any>>src;
+					dest.options = { options: options.map((x) => x[1]) };
+					break;
+				}
+				case "xy": {
+					// replace XY param with two number params
+					fxParams.pop();
+					const labels = "XY";
+					const $src = <XYParam>src;
+					for (let j = 0; j < 2; j++) {
+						const $dest = { ...dest };
+						$dest.id = id + "__" + labels[j];
+						$dest.name = $src.name + ` (${labels[j]})`;
+						$dest.options = { min: 0, max: 1, step: 0.001 };
+						if ($src.default) $dest.default = $src.default[j];
+						fxParams.push($dest);
+						this._adaptations[$dest.id] = this.adaptVectorParam(
+							id,
+							j
+						);
+					}
+				}
+			}
+		}
+		$fx.params(fxParams);
+
+		// only cache adapted param values here
+		// other param types will be cached via .updateParam() above...
+		for (let [id, adaptedParam] of Object.entries(this._adaptations)) {
+			const value = $fx.getParam(id);
+			if (value != null) {
+				this._cache[adaptedParam.id] = adaptedParam.adapt(value);
+			}
+		}
+	}
+
+	setTraits(traits: Traits) {
+		$fx.features(traits);
+	}
+
+	capture(_?: HTMLCanvasElement | SVGElement) {
+		$fx.preview();
+	}
+
+	protected reload() {
+		console.log(
+			`${this.id} reloading with new params:`,
+			this._searchParams.toString()
+		);
+		location.search = this._searchParams.toString();
+	}
+
+	protected initPRNG() {
+		const seed = hashString($fx.hash);
+		const reset = () => sfc32(seed);
+		this._prng = {
+			seed: $fx.hash,
+			rnd: reset(),
+			reset,
+		};
+	}
+
+	protected adaptVectorParam(id: string, idx: number): AdaptedParam {
+		return {
+			id,
+			adapt: (x) => {
+				const value = this._cache[id].slice();
+				value[idx] = x;
+				return value;
+			},
+		};
+	}
+
+	protected warn(msg: string, ...args: any[]) {
+		console.warn(`${this.id}:`, msg, ...args);
+	}
+}
+
+$genart.setAdapter(new FxhashAdapter());
