74af31c7fe81c4f2fcc5ba7c47fa545c4ec740ea
Karsten Schmidt <k@thi.ng>
2025-01-03 20:14:02 +0100

    feat(core): minor update color param impl
    - allow 8-digit color values in validator
    - update coerce() to trim value to 6-digits

--- packages/core/src/params/color.ts
+++ packages/core/src/params/color.ts
@@ -2,7 +2,8 @@ import type { ParamImpl } from "../api/params.js";
 import { isString, u24 } from "../utils.js";
 
 export const color: ParamImpl = {
-	validate: (_, value) => isString(value) && /^#?[0-9a-f]{6}$/.test(value),
-	coerce: (_, value) => (value[0] !== "#" ? "#" + value : value),
+	validate: (_, value) => isString(value) && /^#?[0-9a-f]{6,8}$/i.test(value),
+	coerce: (_, value) =>
+		(value[0] !== "#" ? "#" + value : value).substring(0, 7),
 	randomize: (_, rnd) => "#" + u24((rnd() * 0x1_00_00_00) | 0),
 };
