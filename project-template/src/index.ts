70f7980b3045eff02f8aa7a66bda9113cee3baed
Karsten Schmidt <k@thi.ng>
2025-10-14 11:08:51 +0200

    feat(template): update project template
    - add docs
    - add basic record-mode handling

--- project-template/src/index.ts
+++ project-template/src/index.ts
@@ -1,19 +1,40 @@
 // import type { GenArtAPI } from "@genart-api/core";
 
+// global flag to indicate recording mode
+const RECORD = false;
+
+if (RECORD) {
+	// switch to non-realtime animation when recording
+	$genart.setTimeProvider($genart.time.offline(200, 60));
+}
+
+// declare artwork parameters
+// list of (built-in) supported param types:
+// https://docs.thi.ng/genart-api/core/index.html#parameters
 const param = await $genart.setParams({
 	color: $genart.params.color({
 		name: "Human readable name",
 		desc: "Brief description",
 		// if no default is provided, a random value will be used
 		// default: "#ff0000",
+		// group: "colors",
+		// edit: "private"
+		// see other common param options:
+		// https://docs.thi.ng/genart-api/core/interfaces/ParamOpts.html
 	}),
 });
 
+// set main animation loop, see:
+// https://docs.thi.ng/genart-api/core/types/UpdateFn.html
 $genart.setUpdate((time, frame) => {
 	// draw something...
 	console.log(time, frame);
 	// set background color to current param value
 	document.body.style.backgroundColor = param("color");
+
+	if (RECORD) {
+		// TODO trigger download of current frame (e.g. using thi.ng/dl-asset)
+	}
 
 	// must return true to continue anim loop
 	return true;
